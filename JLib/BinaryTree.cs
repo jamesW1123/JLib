e4eb5b079aff38d27afdb186c85532029320502d
jamesw1123 <jamesw11233@gmail.com>
2020-09-13 08:37:43 -0500

    Defined a Node class.

--- JLib/BinaryTree.cs
+++ JLib/BinaryTree.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace JLib
 {
-    public class BinaryTree
+    public class BinaryTree<T, V>
     {
+        private Node root;
+        private static readonly bool RED = true;
+        private static readonly bool BLACK = false;
+
+
+        private class Node
+        {
+            T key;
+            V value;
+            Node left, right;
+            int N;
+            bool color;
+
+            Node(T key, V value, int N, bool color)
+            {
+                this.key = key;
+                this.value = value;
+                this.N = N;
+                this.color = color;
+            }
+        }
     }
 }
